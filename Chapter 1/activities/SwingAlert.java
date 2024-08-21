ba0e99ece70d07a3468d01c54f70c8a699afba59
John Simon Limosnero <johnleosimone14@gmail.com>
2024-08-21 13:55:22 +0800

    created SwingAlert.java

---
+++ Chapter 1/activities/SwingAlert.java
@@ -0,0 +1,10 @@
+package activities;
+import javax.swing.JOptionPane;
+public class SwingAlert {
+   public static void main(String[] args){
+
+      String greetings = ("Hello World!");
+      
+      JOptionPane.showMessageDialog(null, greetings);
+   }
+}
