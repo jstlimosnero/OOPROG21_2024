c95f43e40e4dfeed684c424de9a1225d1da9b8ad
John Simon Limosnero <johnleosimone14@gmail.com>
2024-09-16 23:00:30 +0800

    Created FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExcercise {
+
+    public static void main(String[] args) {
+    System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Samsung Electronics Co. Ltd.");
+        System.out.println("129 Samsung-Ro, Yeongtong-Gu");
+        System.out.println("Suwon, Seoul 16677");
+        System.out.println("Company: Samsung Electronics Co. Ltd.");
+    }
+}
