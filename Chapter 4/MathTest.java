5385c9bafe529d53750b686fa0173597287980f8
John Simon Limosnero <johnleosimone14@gmail.com>
2024-10-02 22:28:55 +0800

    created MathTest.java

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,15 @@
+public class MathTest {
+      public static void main (String[] args) {
+ 
+       System.out.println("SQRT:" + Math.sqrt (14));
+       System.out.println("SIN:" + Math.sin (14));
+       System.out.println("COS:" + Math.cos (4));
+       System.out.println("FLOOR:" + Math.floor (4));
+       System.out.println("CEIL:" + Math.ceil (11));
+       System.out.println("ROUND:" + Math.round (15));
+       System.out.println("MAX:" + Math.max (100,210));
+       System.out.println("MIN:" + Math.min (100,210));
+       System.out.println("RANDOM:" + Math.random ());
+      
+  }
+}
